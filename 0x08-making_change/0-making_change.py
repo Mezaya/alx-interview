cf9785f26033a158b2753faddf39b8554e364959
mezaya <mesaya@live.com>
2023-05-18 19:07:03 +0200

    change_making

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,6 +3,7 @@
     determine the fewest number of coins needed to meet
     a given amount total.
 '''
+import sys
 
 def makeChange(coins, total):
     """This function will take a list of coins and use
